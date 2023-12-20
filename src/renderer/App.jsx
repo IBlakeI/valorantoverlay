718f7d034ca8331c33c718c585bfdee83d6a18c3
IBlakeI <blake.herren8@gmail.com>
2023-12-19 23:52:30 -0600

    Initial commit

---
+++ src/renderer/App.jsx
@@ -0,0 +1,21 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div className="wrapper">
+      Inital Commit
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
